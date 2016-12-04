4a949ef07be7c455779883241af3b995d9324ce9
Erik Lieben <erik.lieben@effectory.com>
2016-12-04 18:16:48 +0100

    refactor(server): remove server, prepare for re-implementation

--- src/client/main.ts
+++ src/client/main.ts
@@ -20,24 +20,4 @@ export function activate(context: ExtensionContext) {
   invalidCasingAction.activate(context.subscriptions);
   languages.registerCodeActionsProvider('html', invalidCasingAction);
 
-  // Register Aurelia language server
-  const serverModule = context.asAbsolutePath(path.join('dist', 'src', 'server', 'main.js'));
-  const debugOptions = { execArgv: ['--nolazy', '--debug=6004'] };
-  const serverOptions: ServerOptions = {
-    debug: { module: serverModule, options: debugOptions, transport: TransportKind.ipc },
-    run: { module: serverModule, transport: TransportKind.ipc },
-  };
-
-  const clientOptions: LanguageClientOptions = {
-    diagnosticCollectionName: 'Aurelia',
-    documentSelector: ['html'],
-    initializationOptions: {},
-    synchronize: {
-      configurationSection: ['aurelia'],
-    },
-  };
-
-  const client = new LanguageClient('html', 'Aurelia', serverOptions, clientOptions);
-  const disposable = client.start();
-  context.subscriptions.push(disposable);
 }
